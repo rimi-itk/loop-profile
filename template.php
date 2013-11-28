7f792e530065ba479fa0000c2dbb922eb8641534
Rasmus Frey <rasmus@artz.dk>
2013-11-28 14:08:52 +0100

    Added theme files

---
+++ template.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Override or insert variables into the html template.
+ */
+function loop_preprocess_html(&$variables) {
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function loop_preprocess_page(&$variables) {
+}
+
+/**
+ * Override or insert variables into the region template.
+ */
+function loop_preprocess_region(&$variables) {
+}
+
+/**
+ * Override or insert variables into the node template.
+ */
+function loop_preprocess_node(&$variables) {
+}
+
+/**
+ * Override or insert variables into the field template.
+ */
+function loop_preprocess_field(&$variables) {
+}
+
+/**
+ * Override or insert variables into the block template.
+ */
+function loop_preprocess_block(&$variables) {
+}
+
+/**
+ * Override or insert variables into the panel pane template.
+ */
+function loop_preprocess_panels_pane(&$variables) {
+}
