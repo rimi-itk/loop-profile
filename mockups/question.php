aa8416fc5dbd45be78f755d09d98143cca6bb644
Brian Jensen <mail@magnify.dk>
2013-12-09 14:01:40 +0100

    Styling of question

--- mockups/question.php
+++ mockups/question.php
@@ -40,18 +40,31 @@ $path_to_theme = "/profiles/loopdk/themes/loop/";
     </form>
   </div>
 </div>
-<div class="question">
-  <div class="question--inner">
-    <h1><strong>Spørgsmål:</strong> Hvordan dokumenteres delvis aktindsigt? Og findes der en vejledning i dokumentation af aktindsigt?</h1>
-    <ul class="content-list">
-      <li class="content-list--item">
-        <span class="question--answer">Bedste svar:</span> Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </li>
-      <li class="content-list--item is-last">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-      </li>
-    </ul>
-  </div>
+<div class="layout--wrapper">
+  <div class="layout--inner">
+    <section class="question--wrapper">
+      <div class="question--author">
+        <a href="#" class="question--author-image">
+          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+        </a>
+        <a href="#" class="question--author-link">Ribena Hutsitove</a>
+        <div class="question--author-title">SOSU, AUH, HUH</div>
+      </div>
+      <div class="question--meta">
+        <div class="question--meta-date">Oprettet den 27. februar 2013</div>
+        <span class="question--meta-category">Kategori:</span> <a href="#">Dokumentationspraksis</a>
+      </div>
+      <div class="question--inner">
+        <span class="question--header-label">Spørgsmål:</span>
+        <h1 class="question--header">Hvordan dokumenteres delvis aktindsigt? Og findes der en vejledning i dokumentation af aktindsigt?</h1>
+      </div>
+      <div class="question--terms">
+        <a href="#" class="question--term">Stamdata</a>
+        <a href="#" class="question--term">Partsrepræsentant</a>
+        <a href="#" class="question--term">Funktionsvurdering</a>
+      </div>
+    </div>
+  </section>
 </div>
 </body>
 </html>
