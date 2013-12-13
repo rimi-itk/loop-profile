6bc0ebbb1aa5e8d9eac307db0b6b13ef5f544776
Rasmus Frey <rasmus@artz.dk>
2013-12-13 12:55:12 +0100

    Added site header include to all mockups

--- mockups/ask-question.php
+++ mockups/ask-question.php
@@ -6,69 +6,54 @@ $path_to_theme = "/profiles/loopdk/themes/loop/";
 </head>
 
 <body>
-
-<header class="header" role="banner">
-  <div class="header--inner">
-    <a href="/" class="logo--link"><img src="<?php echo $path_to_theme; ?>/logo.png" alt="" class="logo--image"></a>
-    <nav class="nav">
-      <a href="#" title="Min konto" class="nav--link">
-        <i class="icon-user"></i>
-        <span class="nav--text">Min konto</span>
-      </a>
-      <a href="#" title="Notifikationer" class="nav--link-mail">
-        <i class="icon-mail"></i>
-        <span class="nav--text">Notifikationer</span>
-        <span class="notification">3</span>
-      </a>
-      <a href="#" title="Menu" class="nav--link-menu">
-        <i class="icon-menu"></i>
-        <span class="nav--text">Menu</span>
-      </a>
-    </nav>
-  </div>
-</header>
-<?php include('inc/search-block.inc') ?>
-<div class="layout--wrapper">
-  <div class="layout--inner">
-    <h1 class="page-title">Opret spørgsmål</h1>
-    <p>
-      Udfyld formularen for at oprette dit spørgsmål. Husk at brug søgefunktionen først, så du ikke opretter et spørgsmål der allerede er blevet oprettet (og evt. besvaret).
-    </p>
-    <div class="form-module">
-      <form action="ask-question.php">
-        <div class="form-item">
-          <label for="question-categori">Kategori <span class="form-required">*</span></label>
-          <select class="form-select" required>
-            <option value="">Vælg kategori</option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-          </select>
-        </div>
-        <div class="form-item">
-          <label for="question-terms">Nøgleord <span class="form-required">*</span></label>
-          <div class="at-term-list">
-            <div class="at-term at-term-remove activeTagsRemove-processed">
-              <span class="at-term-text">Afgørelser</span>
+<div class="page-wrapper js-page-wrapper">
+  <div class="page-inner">
+    <?php include 'inc/header.inc'; ?>
+    <?php include('inc/search-block.inc') ?>
+    <div class="layout--wrapper">
+      <div class="layout--inner">
+        <h1 class="page-title">Opret spørgsmål</h1>
+        <p>
+          Udfyld formularen for at oprette dit spørgsmål. Husk at brug søgefunktionen først, så du ikke opretter et spørgsmål der allerede er blevet oprettet (og evt. besvaret).
+        </p>
+        <div class="form-module">
+          <form action="ask-question.php">
+            <div class="form-item">
+              <label for="question-categori">Kategori <span class="form-required">*</span></label>
+              <select class="form-select" required>
+                <option value="">Vælg kategori</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
             </div>
-            <div class="at-term at-term-remove activeTagsRemove-processed">
-              <span class="at-term-text">Optag lås</span>
+            <div class="form-item">
+              <label for="question-terms">Nøgleord <span class="form-required">*</span></label>
+              <div class="at-term-list">
+                <div class="at-term at-term-remove activeTagsRemove-processed">
+                  <span class="at-term-text">Afgørelser</span>
+                </div>
+                <div class="at-term at-term-remove activeTagsRemove-processed">
+                  <span class="at-term-text">Optag lås</span>
+                </div>
+              </div>
+              <input type="text" placeholder="Indtast nøgleord, systemet kommer automatisk med forslag" class="at-term-entry form-text" id="question-terms" required>
+              <input type="submit" value="Tilføj nøgleord" class="at-add-btn">
+              <div class="form-item-description">Indtast nøgleord, systemet kommer automatisk med forslag. Tryk på enter eller knappen for at tilføje.</div>
             </div>
-          </div>
-          <input type="text" placeholder="Indtast nøgleord, systemet kommer automatisk med forslag" class="at-term-entry form-text" id="question-terms" required>
-          <input type="submit" value="Tilføj nøgleord" class="at-add-btn">
-          <div class="form-item-description">Indtast nøgleord, systemet kommer automatisk med forslag. Tryk på enter eller knappen for at tilføje.</div>
+            <div class="form-item">
+              <label for="question-textarea">Spørgsmål <span class="form-required">*</span></label>
+              <textarea rows="1" class="form-textarea" id="question-textarea" required></textarea>
+              <div class="form-item-description">Enter your question or observation.</div>
+            </div>
+            <div class="form-actions">
+              <input type="submit" value="Opret spørgsmål" class="form-submit">
+            </div>
+          </form>
         </div>
-        <div class="form-item">
-          <label for="question-textarea">Spørgsmål <span class="form-required">*</span></label>
-          <textarea rows="1" class="form-textarea" id="question-textarea" required></textarea>
-          <div class="form-item-description">Enter your question or observation.</div>
-        </div>
-        <div class="form-actions">
-          <input type="submit" value="Opret spørgsmål" class="form-submit">
-        </div>
-      </form>
+      </div>
     </div>
   </div>
+</div>
 </body>
 </html>
--- mockups/question.php
+++ mockups/question.php
@@ -7,154 +7,139 @@ $path_to_theme = "/profiles/loopdk/themes/loop/";
 
 <body>
 
-<header class="header" role="banner">
-  <div class="header--inner">
-    <a href="/" class="logo--link"><img src="<?php echo $path_to_theme; ?>/logo.png" alt="" class="logo--image"></a>
-    <nav class="nav">
-      <a href="#" title="Min konto" class="nav--link">
-        <i class="icon-user"></i>
-        <span class="nav--text">Min konto</span>
-      </a>
-      <a href="#" title="Notifikationer" class="nav--link-mail">
-        <i class="icon-mail"></i>
-        <span class="nav--text">Notifikationer</span>
-        <span class="notification">3</span>
-      </a>
-      <a href="#" title="Menu" class="nav--link-menu">
-        <i class="icon-menu"></i>
-        <span class="nav--text">Menu</span>
-      </a>
-    </nav>
-  </div>
-</header>
-<?php include('inc/search-block.inc') ?>
-<div class="layout--wrapper">
-  <div class="layout--inner">
-    <section class="question--wrapper">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+<div class="page-wrapper js-page-wrapper">
+  <div class="page-inner">
+    <?php include 'inc/header.inc'; ?>
+    <?php include('inc/search-block.inc') ?>
+    <div class="layout--wrapper">
+      <div class="layout--inner">
+        <section class="question--wrapper">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <div class="question--meta-data">
+            <div class="question--meta-data-date">Oprettet den 27. februar 2013</div>
+            <span class="question--meta-data-category">Kategori:</span> <a href="#">Dokumentationspraksis</a>
+          </div>
+          <div class="question--inner">
+            <span class="question--label">Spørgsmål:</span>
+            <h1 class="question">Hvordan dokumenteres delvis aktindsigt? Og findes der en vejledning i dokumentation af aktindsigt?</h1>
+          </div>
+          <div class="question--terms">
+            <a href="#" class="question--term">Stamdata</a>
+            <a href="#" class="question--term">Partsrepræsentant</a>
+            <a href="#" class="question--term">Funktionsvurdering</a>
+          </div>
+        </section>
+        <div class="comments--comment">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <a href="#" class="flag is-sticky">Mest nyttige svar</a>
+          <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
+          <div class="comments--comment-content">
+            Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
+          </div>
+        </div>
+        <div class="comments--comment">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
+          <div class="comments--comment-content">
+            Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
+          </div>
+          <a href="#" class="flag">Dette svar er nyttigt (12)</a>
+        </div>
+        <div class="comments--comment">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
+          <div class="comments--comment-content">
+            Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
+          </div>
+          <a href="#" class="flag">Dette svar er nyttigt (0)</a>
+        </div>
+        <div class="comments--comment">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
+          <div class="comments--comment-content">
+            Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
+          </div>
+          <a href="#" class="flag is-active">Du fandt dette svar nyttigt</a>
+        </div>
+        <div class="comments--comment is-last">
+          <div class="meta-data--author">
+            <a href="#" class="meta-data--author-image">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+            </a>
+            <div class="meta-data--author-wrapper">
+              <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
+              <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+            </div>
+          </div>
+          <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
+          <div class="comments--comment-content">
+            Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
+          </div>
+          <a href="#" class="flag">Dette svar er nyttigt (3)</a>
         </div>
       </div>
-      <div class="question--meta-data">
-        <div class="question--meta-data-date">Oprettet den 27. februar 2013</div>
-        <span class="question--meta-data-category">Kategori:</span> <a href="#">Dokumentationspraksis</a>
-      </div>
-      <div class="question--inner">
-        <span class="question--label">Spørgsmål:</span>
-        <h1 class="question">Hvordan dokumenteres delvis aktindsigt? Og findes der en vejledning i dokumentation af aktindsigt?</h1>
-      </div>
-      <div class="question--terms">
-        <a href="#" class="question--term">Stamdata</a>
-        <a href="#" class="question--term">Partsrepræsentant</a>
-        <a href="#" class="question--term">Funktionsvurdering</a>
-      </div>
-    </section>
-    <div class="comments--comment">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+      <div class="layout--inner">
+        <div class="meta-data--author">
+          <a href="#" class="meta-data--author-image">
+            <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
+          </a>
+          <div class="meta-data--author-wrapper">
+            Ribena Hutsitove
+            <span class="meta-data--author-title">SOSU, AUH, HUH</span>
+          </div>
+        </div>
+        <div class="form-module">
+          <form action="ask-question.php">
+            <div class="form-item">
+              <label for="comment-textarea">Skriv svar <span class="form-required">*</span></label>
+              <textarea rows="5" class="form-textarea required" id="comment-textarea"></textarea>
+            </div>
+            <div class="form-actions">
+              <input type="submit" value="Skriv svar" class="form-submit">
+            </div>
+          </form>
         </div>
       </div>
-      <a href="#" class="flag is-sticky">Mest nyttige svar</a>
-      <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
-      <div class="comments--comment-content">
-        Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </div>
-    </div>
-    <div class="comments--comment">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
-        </div>
-      </div>
-      <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
-      <div class="comments--comment-content">
-        Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </div>
-      <a href="#" class="flag">Dette svar er nyttigt (12)</a>
-    </div>
-    <div class="comments--comment">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
-        </div>
-      </div>
-      <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
-      <div class="comments--comment-content">
-        Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </div>
-      <a href="#" class="flag">Dette svar er nyttigt (0)</a>
-    </div>
-    <div class="comments--comment">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
-        </div>
-      </div>
-      <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
-      <div class="comments--comment-content">
-        Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </div>
-      <a href="#" class="flag is-active">Du fandt dette svar nyttigt</a>
-    </div>
-    <div class="comments--comment is-last">
-      <div class="meta-data--author">
-        <a href="#" class="meta-data--author-image">
-          <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-        </a>
-        <div class="meta-data--author-wrapper">
-          <a href="#" class="meta-data--author-link">Ribena Hutsitove</a>
-          <span class="meta-data--author-title">SOSU, AUH, HUH</span>
-        </div>
-      </div>
-      <span class="comments--comment-meta-data">Oprettet den 28. marts 2013</span>
-      <div class="comments--comment-content">
-        Nej der er ikke en selvstændig vejledning i aktindsigt. Under aktindsigt (i Stamdata) kan Status sættes til Delvis bevilliget samt i bemærkningen kan dokumenteres hvilken sag det omfatter.
-      </div>
-      <a href="#" class="flag">Dette svar er nyttigt (3)</a>
-    </div>
-  </div>
-  <div class="layout--inner">
-    <div class="meta-data--author">
-      <a href="#" class="meta-data--author-image">
-        <img src="https://s3.amazonaws.com/uifaces/faces/twitter/jackiesaik/128.jpg">
-      </a>
-      <div class="meta-data--author-wrapper">
-        Ribena Hutsitove
-        <span class="meta-data--author-title">SOSU, AUH, HUH</span>
-      </div>
-    </div>
-    <div class="form-module">
-      <form action="ask-question.php">
-        <div class="form-item">
-          <label for="comment-textarea">Skriv svar <span class="form-required">*</span></label>
-          <textarea rows="5" class="form-textarea required" id="comment-textarea"></textarea>
-        </div>
-        <div class="form-actions">
-          <input type="submit" value="Skriv svar" class="form-submit">
-        </div>
-      </form>
     </div>
   </div>
 </div>
