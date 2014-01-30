5cc6ee6069187f9b36902817b2f9d8d3abc0b63b
Brian Jensen <hallo@magnify.dk>
2014-01-30 14:47:01 +0100

    Refactored layout and added layout mockup page

---
+++ mockups/layouts.php
@@ -0,0 +1,25 @@
+<?php
+$path_to_theme = "/profiles/loopdk/themes/loop/";
+?>
+<?php include('inc/head.inc') ?>
+<title>LOOP - layouts</title>
+</head>
+
+<body>
+
+<div class="page-wrapper js-page-wrapper">
+  <div class="page-inner">
+    <?php include 'inc/header.inc'; ?>
+    <?php include('inc/search-block.inc') ?>
+    <h1 style="border-bottom: 1px solid gray; margin-right: .5em; margin: 0 0 1em; margin-left: .5em; padding-bottom: .5em; text-align: center;">Ask question</h1>
+    <?php include('inc/page-ask-question.inc') ?>
+    <h1 style="border-bottom: 1px solid gray; margin-right: .5em; margin: 1em ; margin-left: .5em; padding-bottom: .5em; text-align: center;">Question</h1>
+    <?php include('inc/page-question.inc') ?>
+    <h1 style="border-bottom: 1px solid gray; margin-right: .5em; margin: 1em 0; margin-left: .5em; padding-bottom: .5em; text-align: center;">Search results</h1>
+    <?php include('inc/page-search-result.inc') ?>
+    <h1 style="border-bottom: 1px solid gray; margin-right: .5em; margin: 1em 0; margin-left: .5em; padding-bottom: .5em; text-align: center;">No search results</h1>
+    <?php include('inc/page-no-search-results.inc') ?>
+  </div>
+</div>
+</body>
+</html>
--- mockups/search-result.php
+++ mockups/search-result.php
@@ -11,27 +11,7 @@ $path_to_theme = "/profiles/loopdk/themes/loop/";
   <div class="page-inner">
     <?php include 'inc/header.inc'; ?>
     <?php include('inc/search-block.inc') ?>
-    <div class="layout--wrapper">
-      <div class="layout--inner">
-        <h1 class="page-title">Søgeresultater</h1>
-        <div class="search-result">
-          <div class="search-result--lead">
-            Du søgte på: <strong>Dokumentation</strong>
-          </div>
-          <?php
-            for ($i=1;$i<=10;$i++) {
-              echo '
-          <div class="search-result--item">
-            <span class="meta-data--date">Oprettet den 28. marts 2013</span>
-            <a href="question.php" class="search-result--link">Hvordan dokumenteres delvis aktindsigt? Og findes der en vejledning i dokumentation af aktindsigt?</a>
-            <a href="#" class="search-result--comments">12 svar</a>
-          </div>
-              ';
-            }
-          ?>
-        </div>
-      </div>
-    </div>
+    <?php include('inc/page-search-result.inc') ?>
   </div>
 </div>
 </body>
