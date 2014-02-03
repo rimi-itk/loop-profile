535ed139567db629ee204ab8116ce76ffe112f84
Thomas Johansen <thsj@aarhus.dk>
2014-02-03 16:09:59 +0100

    Updated typeahead version

--- loop_search.js
+++ loop_search.js
@@ -3,35 +3,51 @@
  * Autocomplete attachement for search in Apache Solr.
  */
 
-
 /**
- * Adds the custom autocomplete widget behavior.
+ * Default ready function.
+ *
+ * Prefetch nodes.
+ * Start typeahead.
  */
-Drupal.behaviors.loop_search = {
-  attach: function(context) {
-    jQuery('.typeahead', context).typeahead(
-      {
-        // The autocomplete url.
-        remote: '/loop_search_autocomplete/%QUERY',
-
-        // Name of search.
-        name: 'search',
-
-        // The template we are building suggestion list from.
-        template: [
-          '<span>{{link}}</span>',
-          '<span>{{value}}</span>',
-          '<span>{{suggestion}}</span>'
-        ].join(''),
-
-        // Template enigne.
-        engine: Hogan
-      }
-    );
-  }
-};
-
 jQuery(document).ready(function($) {
+  var testing = new Bloodhound({
+    datumTokenizer: function(d) {
+      // Tokenize by splitting whitespace.
+      return Bloodhound.tokenizers.whitespace(d.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch:  {
+      // URL for prefetch!
+      url: '/loop_search_nodes'
+    },
+    name: 'search'
+  });
+
+  testing.initialize();
+
+  jQuery('.typeahead').typeahead(null,
+    {
+      // Bloodhound source.
+      source: testing.ttAdapter(),
+
+      // Name of search.
+      name: 'search',
+
+      // Display field.
+      displayKey: 'title',
+
+      // The template we are building suggestion list from.
+      template: [
+        '<span>{{link}}</span>',
+        '<span>{{title}}</span>'
+      ].join(''),
+
+      // Template enigne.
+      engine: Hogan
+    }
+  );
+
+
   $('.typeahead').on('typeahead:selected', function (object, datum) {
     // If suggestion contains a link. Redirect.
     if (datum['link'] != undefined) {
@@ -39,8 +55,7 @@ jQuery(document).ready(function($) {
     }
     else {
       // Suggestion is clicked. Display the results.
-      $('.typeahead').blur();
-      $('.typeahead').focus();
+      $('.typeahead').blur().focus();
     }
   });
 });
