b949a819fb27ece6d0ee2224c9116dda0b1c05a9
Martin Yde <my@aarhus.dk>
2014-02-05 15:15:53 +0100

    Added new pages to user profile

--- templates/loop-user--my-content.tpl.php
+++ templates/loop-user--my-content.tpl.php
@@ -10,21 +10,23 @@
  *
  */
 ?>
-<?php if ($user->uid == $profile->uid) : ?>
-  <?php $title = t('My content'); ?>
-  <?php $question_title = t('My questions'); ?>
-  <?php $comments_title = t('My comments'); ?>
-<?php else : ?>
-  <?php $title = t('Users content'); ?>
-  <?php $question_title = t('Questions'); ?>
-  <?php $comments_title = t('Comments'); ?>
+<?php if (!empty($profile)) : ?>
+  <?php if ($user->uid == $profile->uid) : ?>
+    <?php $title = t('My content'); ?>
+    <?php $question_title = t('My questions'); ?>
+    <?php $comments_title = t('My comments'); ?>
+  <?php else : ?>
+    <?php $title = t('Users content'); ?>
+    <?php $question_title = t('Questions'); ?>
+    <?php $comments_title = t('Comments'); ?>
+  <?php endif; ?>
+  <aside class="block-my-content">
+    <div class="block-module--inner">
+      <h2 class="block-module--my-content-header"><?php print $title;?> </h2>
+      <ul class="block-module--my-content-list">
+        <li class="block-module--my-content-item"><?php echo l($question_title, 'user/' . $profile->uid . '/nodes', array('attributes' => array('class' => array('block-module--link'))));?></li>
+        <li class="block-module--my-content-item"><?php echo l($comments_title, 'user/' . $profile->uid . '/user-comments', array('attributes' => array('class' => array('block-module--link'))));?></li>
+      </ul>
+    </div>
+  </aside>
 <?php endif; ?>
-<aside class="block-my-content">
-  <div class="block-module--inner">
-    <h2 class="block-module--my-content-header"><?php print $title;?> </h2>
-    <ul class="block-module--my-content-list">
-      <li class="block-module--my-content-item"><?php echo l($question_title, 'user/' . $profile->uid . '/nodes', array('attributes' => array('class' => array('block-module--link'))));?></li>
-      <li class="block-module--my-content-item"><?php echo l($comments_title, 'user/' . $profile->uid . '/user-comments', array('attributes' => array('class' => array('block-module--link'))));?></li>
-    </ul>
-  </div>
-</aside>
