0d1bce91d764b6253ca40f186c399ef8aa80b0c2
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-04 09:57:44 +0100

    Added partial fix to handle crossreferences

--- Leaf.php
+++ Leaf.php
@@ -11,4 +11,8 @@ class Leaf extends LoopNode {
   public function getBody() {
     return $this->body;
   }
+
+  public function setBody($body) {
+    $this->body = $body;
+  }
 } 
--- LoopIndex.php
+++ LoopIndex.php
@@ -2,12 +2,18 @@
 
 class LoopIndex {
   private $children;
+  private $references;
 
-  public function __construct($children) {
+  public function __construct($children, $references) {
     $this->children = $children;
+    $this->references = $references;
   }
 
   public function getChildren() {
     return $this->children;
   }
-} 
+
+  public function getReferences() {
+    return $this->references;
+  }
+}
