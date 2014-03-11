a4f69bd8eb02801c9e6f257061bc166e46cf8226
Brian Jensen <hallo@magnify.dk>
2014-03-11 15:45:08 +0100

    Added header

--- templates/loop-external-data-dropdown-block.tpl.php
+++ templates/loop-external-data-dropdown-block.tpl.php
@@ -1,4 +1,5 @@
 <div class="guide--nav-wrapper">
+  <h2><?php print t('Index'); ?></h2>
   <div class="guide--nav-wrapper-inner">
     <?php foreach ($forest['childs'] as $tree): ?>
       <?php print render($tree); ?>
