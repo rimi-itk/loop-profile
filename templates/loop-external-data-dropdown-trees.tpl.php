e80143c2dca9bc82c43b6c4f81018765d118040d
Brian Jensen <hallo@magnify.dk>
2014-03-13 13:21:58 +0100

    Changed list classes

--- templates/loop-external-data-dropdown-trees.tpl.php
+++ templates/loop-external-data-dropdown-trees.tpl.php
@@ -1,5 +1,6 @@
-<li class="guide--nav-list-item-sublist"><?php print $title; ?>
-  <ul class="guide--nav-list">
+<li class="guide--nav-list-item has-sublist">
+  <h4 class="guide--nav-list-title"><?php print $title; ?></h4>
+  <ul class="guide--nav-list-sublist">
     <?php foreach ($childs as $child): ?>
       <?php print render($child); ?>
     <?php endforeach; ?>
