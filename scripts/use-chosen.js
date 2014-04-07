797cbe9c6ccfab0d721a07171601de33b3fbf294
Martin Yde <my@aarhus.dk>
2014-04-07 11:03:41 +0200

    Added chosen on multiselect.

---
+++ scripts/use-chosen.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: martinydegranath
+ * Date: 07/04/14
+ * Time: 10.22
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function($) {
+  // Start the show.
+  $(document).ready(function () {
+    $('.chosen-select').chosen();
+  });
+
+})(jQuery);
