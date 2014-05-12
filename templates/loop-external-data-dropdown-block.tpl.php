e80192a4bed4b1f5d7a426f76ab4ce3d95d267ee
Troels Ugilt Jensen <tujensen@gmail.com>
2014-05-12 14:44:30 +0200

    Removed Index text, replace with guide title

--- templates/loop-external-data-dropdown-block.tpl.php
+++ templates/loop-external-data-dropdown-block.tpl.php
@@ -1,5 +1,5 @@
 <div class="guide--nav-wrapper">
-  <h2><?php print t('Index'); ?></h2>
+  <h2><?php print $forest['index_title']; ?></h2>
   <div class="guide--nav-wrapper-inner">
     <ul class="guide--nav-list">
       <?php foreach ($forest['childs'] as $tree): ?>
