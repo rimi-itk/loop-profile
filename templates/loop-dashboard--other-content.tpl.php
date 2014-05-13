0c2048d434a1f5da17e8544d4c88a38231b4ab1a
Troels Ugilt Jensen <tujensen@gmail.com>
2014-05-13 13:58:00 +0200

    Added templates and database calls

---
+++ templates/loop-dashboard--other-content.tpl.php
@@ -0,0 +1,91 @@
+<div class="view view-loop-editor-content view-id-loop_editor_content view-display-id-panel_pane_1 view-dom-id-4389197b767e4d095d10198b4f63313f contextual-links-region jquery-once-1-processed">
+  <div class="contextual-links-wrapper contextual-links-processed"><a class="contextual-links-trigger" href="#">Konfigurér</a><ul class="contextual-links"><li class="views-ui-edit first last"><a href="/admin/structure/views/view/loop_editor_content/edit/panel_pane_1?destination=editor/dashboard">Rediger oversigt</a></li>
+    </ul></div>
+  <div class="dashboard-list--filter">
+    <form class="dashboard-list--form jquery-once-1-processed" action="/editor/dashboard/content" method="get" id="views-exposed-form-loop-editor-content-panel-pane-1" accept-charset="UTF-8"><div><div class="views-exposed-form">
+          <div class="views-exposed-widgets">
+            <div id="edit-combine-wrapper" class="views-exposed-widget views-widget-filter-combine">
+              <div class="views-widget">
+                <div class="form-item form-type-textfield form-item-combine">
+                  <input placeholder="Skriv dele af titel eller indhold for at filtrere listen" class="dashboard-list--filter-field form-text" type="text" id="edit-combine" name="combine" value="" size="30" maxlength="128">
+                </div>
+              </div>
+            </div>
+            <div class="views-exposed-widget views-submit-button">
+              <input class="dashboard-list--submit form-submit" type="submit" id="edit-submit-loop-editor-content" name="" value="Udfør">    </div>
+          </div>
+        </div>
+      </div></form>      <div class="dashboard--comment-filters">
+      <label class="dashboard--filter-label">Show only</label>
+      <div class="dashboard--filter-links">
+        <a href="#" class="dashboard--filter-link js-dashboard-answers-hide is-active">Unanswered questions</a>          <a href="#" class="dashboard--filter-link js-dashboard-answers-show">Answered questions</a>        </div>
+    </div>
+    <div class="dashboard--sorting">
+      <label class="dashboard--filter-label">Sortér efter</label>
+      <div class="dashboard--sort-links">
+        <a href="#" class="dashboard--sort-link is-active js-sort-link">Newest</a>          <a href="#" class="dashboard--sort-link js-sort-link">Oldest</a>          <a href="#" class="dashboard--sort-link js-sort-link js-dashboard-alphabetical">Alphabetical</a>          <a href="#" class="dashboard--sort-link js-dashboard-answers">Number of answers</a>        </div>
+    </div>
+  </div>
+
+  <div class="dashboard-list">
+    <div class="dashboard-list--item">
+
+      <a href="/node/433/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/433">Hvordan kan information om egen læge ændres?</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    30. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/432/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/432">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    30. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/431/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/431">Tester lige at oprette et nyt spørgsmål</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    24. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/430/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/430">time for another message test</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    8. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/429/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/429">test</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    8. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/428/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/428">test</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    8. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/427/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/427">New post</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    8. april 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/426/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/426">Findes der retningslinier for hvordan man skal disponere i forhold til sygepl. studerende? Jeg har fra en kollega fået afvide at sygepl. studerende ikke må stå alene med besøg i disponering. Noget med at det ikke er juridsk korrekt at de står alene til be</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    21. marts 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/424/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/424">Må der stå andet under nødvendige oplysninger end det vejledningen angiver? Fx Grib Hverdagen eller Palliativt forløb med opstartsdatoer?</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    19. marts 2014  </span>  </div>
+    <div class="dashboard-list--item">
+
+      <a href="/node/423/edit" class="dashboard-list--icon"> </a>
+      <p class="dashboard-list--text">        <a href="/node/423">hvor dokumenteres pårørende</a>  </p>
+      <span class="dashboard-list--meta-data">    Indhold, oprettet    18. marts 2014  </span>  </div>
+  </div>
+
+
+
+  <div class="dashboard-list--more-link">
+    <a href="/editor/dashboard/content?combine=">
+      vis alt indhold</a>
+  </div>
+
+
+</div>
