1cbdd8f9e01256df7591c8fa6c88a62ef1a985c4
Troels Ugilt Jensen <tujensen@gmail.com>
2014-05-14 14:56:36 +0200

    Added filters for other content

--- templates/loop-dashboard--other-content.tpl.php
+++ templates/loop-dashboard--other-content.tpl.php
@@ -2,7 +2,7 @@
   <div class="dashboard-list--filter">
     <form class="dashboard-list--form jquery-once-1-processed" action="/editor/dashboard/content" method="get" id="views-exposed-form-loop-editor-content-panel-pane-1" accept-charset="UTF-8">
       <div class="views-exposed-widgets">
-        <input placeholder="<?php print t('Type parts of title or content to filter the list');?>" class="dashboard-list--filter-field form-text" type="text" id="edit-combine" name="combine" value="" size="30" maxlength="128">
+        <input placeholder="<?php print t('Type parts of title or content to filter the list');?>" class="dashboard-list--filter-field form-text js-other-text-filter" type="text" id="edit-combine" name="combine" value="" size="30" maxlength="128">
       </div>
       <div class="">
         <input class="dashboard-list--submit form-submit" type="submit" id="edit-submit-loop-editor-content" name="" value="Udfør">
@@ -11,14 +11,14 @@
     <div class="dashboard--sorting">
       <label class="dashboard--filter-label"><?php print t('Sort by');?></label>
       <div class="dashboard--sort-links">
-        <a href="#" class="dashboard--sort-link is-active js-sort-link"><?php print t('Newest');?></a>
-        <a href="#" class="dashboard--sort-link js-sort-link"><?php print t('Oldest');?></a>
-        <a href="#" class="dashboard--sort-link js-sort-link js-dashboard-alphabetical"><?php print t('Alphabetical');?></a>
+        <a href="#" class="js-has-answers-removed dashboard--sort-link js-other-sort-filter js-other-sort-newest is-active"><?php print t('Newest');?></a>
+        <a href="#" class="js-has-answers-removed dashboard--sort-link js-other-sort-filter js-other-sort-oldest "><?php print t('Oldest');?></a>
+        <a href="#" class="js-has-answers-removed dashboard--sort-link js-other-sort-filter js-other-sort-alphabetic is-last"><?php print t('Alphabetical');?></a>
       </div>
     </div>
   </div>
 
-  <div class="dashboard-list js-dashboard-other-content">
+  <div class="dashboard-list js-dashboard-other">
     <div class="dashboard--spinner">
     </div>
   </div>
@@ -32,7 +32,7 @@
 <?php
 // This is used to enrich the above results.
 ?>
-<script id="js-list-item-template-other-content" type="text/x-handlebars-template">
+<script id="js-list-item-template-other" type="text/x-handlebars-template">
   <div class="dashboard-list--item">
     <a href="/node/{{nid}}/edit" class="dashboard-list--icon"> </a>
     <p class="dashboard-list--text">
