9fa52d48200c2b3740d662b883cc74471e36be56
martinyde <yde001@gmail.com>
2014-05-14 10:02:23 +0200

    Added spinner

--- templates/loop-dashboard--questions.tpl.php
+++ templates/loop-dashboard--questions.tpl.php
@@ -27,7 +27,8 @@
   </div>
 
   <div class="dashboard-list js-dashboard-questions">
-    Insert here
+    <div class="dashboard--spinner">
+    </div>
   </div>
 
   <div class="dashboard-list--more-link">
