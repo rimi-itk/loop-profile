9509430a110d8183703d9581294ed818c46d1a42
Jesper Kristensen <cableman@linuxdev.dk>
2014-11-13 12:53:14 +0100

    Issue #loop-65 by cableman: Started on new statistic queries and ouputted the first tables

---
+++ templates/loop_admin_stats_post_count.tpl.php
@@ -0,0 +1,5 @@
+<div>
+  <p><span><?php print $post_count; ?></span> <?php print t('new questions in the periode'); ?></p>
+  <p><span><?php print $comment_count; ?></span> <?php print t('new answers in the periode'); ?>
+  <p><a href="#">Google analytices</a></p>
+</div>
