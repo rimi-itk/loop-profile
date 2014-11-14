b184a3c6f8274b12bbaf09c7c08a968254f482d2
martinyde <yde001@gmail.com>
2014-11-14 12:08:25 +0100

    Martin Yde - #loop-70, Added styles for overview

--- templates/loop_admin_stats_post_count.tpl.php
+++ templates/loop_admin_stats_post_count.tpl.php
@@ -1,5 +1,7 @@
 <div class="dashboard--content-overview">
-  <p class="dashboard--content-overview-text"><span><?php print $post_count; ?></span> <?php print t('new questions in the periode'); ?></p>
-  <p class="dashboard--content-overview-text"><span><?php print $comment_count; ?></span> <?php print t('new answers in the periode'); ?></p>
-  <p class="dashboard--content-overview-link"><a href="http://www.google.dk/intl/da/analytics/" target="_blank">Google analytices</a></p>
+  <div class="dashboard--content-overview-inner">
+    <p class="dashboard--content-overview-text"><span><?php print $post_count; ?></span> <?php print t('new questions in the periode'); ?></p>
+    <p class="dashboard--content-overview-text"><span><?php print $comment_count; ?></span> <?php print t('new answers in the periode'); ?></p>
+    <p class="dashboard--content-overview-link"><a href="http://www.google.dk/intl/da/analytics/" target="_blank">Google analytices</a></p>
+  </div>
 </div>
