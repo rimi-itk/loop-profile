cdbb54581e37823086e6d3624a80d0728a9fcf3a
martinyde <yde001@gmail.com>
2014-12-15 13:20:48 +0100

    Martin Yde - #Support-102, alert paths

--- js/loop_search.js
+++ js/loop_search.js
@@ -54,10 +54,6 @@ jQuery(document).ready(function($) {
  *   Hostname of full link.
  */
 function url_domain(data) {
-  var full_host = location.protocol + '//' + location.host;
-  if (data.indexOf('http') < 0) {
-    alert(data);
-  }
   var a = document.createElement('a');
   a.href = data;
   alert(a.hostname);
