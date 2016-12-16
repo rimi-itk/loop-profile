0061a1d6faa90345248f165a19ffd34fe4c62c62
Martin Yde Granath <my@aarhus.dk>
2016-12-16 10:57:13 +0100

    Added initial-scale=1.0 to metatag

--- themes/loop/templates/html.tpl.php
+++ themes/loop/templates/html.tpl.php
@@ -48,7 +48,7 @@
   <?php print $head; ?>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <!--[if IE]>
