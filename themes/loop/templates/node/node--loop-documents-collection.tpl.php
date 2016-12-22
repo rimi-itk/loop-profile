e4551fde6823325ebed50abeaebee917e6839724
Mikkel Ricky <rimi@aarhus.dk>
2016-12-22 14:50:39 +0100

    LOOP-387: Added metadata display to template

--- themes/loop/templates/node/node--loop-documents-collection.tpl.php
+++ themes/loop/templates/node/node--loop-documents-collection.tpl.php
@@ -6,15 +6,40 @@
  */
 ?>
 <div class="loop-documents loop-documents--collection">
-  <div class="loop-documents--navigation loop-documents--collection-navigation guide--nav-wrapper">
-    <?php
-		if (isset($loop_documents_menu)) {
-			echo render($loop_documents_menu);
-		}
-		?>
+  <div class="loop-documents--navigation">
+    <?php if (!empty($loop_documents_menu)): ?>
+      <div class="loop-documents--collection-navigation guide--nav-wrapper">
+        <?php
+        if (isset($loop_documents_menu)) {
+          echo '<h2>', t('Contents'), '</h2>';
+
+          echo render($loop_documents_menu);
+        }
+        ?>
+      </div>
+    <?php endif ?>
   </div>
 
-  <div class="loop-documents--content loop-documents--collection-content">
-    <?php include drupal_get_path('theme', 'loop') . '/templates/node/node--page.tpl.php'; ?>
+  <div class="loop-documents--content">
+    <div class="loop-documents--collection-content">
+      <?php include drupal_get_path('theme', 'loop') . '/templates/node/node--page.tpl.php'; ?>
+    </div>
+
+    <div class="loop-documents--metadata">
+      <fieldset>
+        <legend><?php echo t('Metadata'); ?></legend>
+        <?php foreach (array(
+          'field_loop_documents_owner',
+          'field_loop_documents_version',
+          'field_loop_documents_approver',
+          'field_loop_documents_approv_date',
+          'field_loop_documents_review_date',
+        ) as $field_name) {
+          $field = field_view_field('node', $node, $field_name);
+          echo render($field);
+        }
+        ?>
+      </fieldset>
+    </div>
   </div>
 </div>
--- themes/loop/templates/node/node--loop-documents-document.tpl.php
+++ themes/loop/templates/node/node--loop-documents-document.tpl.php
@@ -6,53 +6,59 @@
  */
 ?>
 <div class="loop-documents loop-documents--document">
-  <div class="loop-documents--navigation loop-documents--document-navigation guide--nav-wrapper">
-    <?php
-    if (isset($loop_documents_menu)) {
-			if (isset($loop_documents_menu['#root'])) {
-				$root = $loop_documents_menu['#root'];
-				echo '<h2>';
-				echo $root->title;
+  <div class="loop-documents--navigation">
+    <?php if (!empty($loop_documents_menu) || !empty($loop_documents_roots)): ?>
+      <div class="loop-documents--document-navigation guide--nav-wrapper">
+        <?php
+        if (!empty($loop_documents_menu)) {
+          if (isset($loop_documents_menu['#root'])) {
+            $root = $loop_documents_menu['#root'];
+            echo '<h2>';
+            echo $root->title;
 
-				if (isset($loop_documents_menu['#root_edit_link'])) {
-					echo ' (' . $loop_documents_menu['#root_edit_link'] . ')';
-				}
-				echo '</h2>';
+            if (isset($loop_documents_menu['#root_edit_link'])) {
+              echo ' (' . $loop_documents_menu['#root_edit_link'] . ')';
+            }
+            echo '</h2>';
 
-				echo '<fieldset><legend>' . t('Metadata') . '</legend>';
-				foreach (array(
-					'field_loop_documents_owner',
-					'field_loop_documents_version',
-					'field_loop_documents_approver',
-					'field_loop_documents_approv_date',
-					'field_loop_documents_review_date',
-				) as $field_name) {
-					$field = field_view_field('node', $root, $field_name);
-					echo render($field);
-				}
-				echo '</fieldset>';
-			}
+            echo '<fieldset><legend>' . t('Metadata') . '</legend>';
+            foreach (array(
+              'field_loop_documents_owner',
+              'field_loop_documents_version',
+              'field_loop_documents_approver',
+              'field_loop_documents_approv_date',
+              'field_loop_documents_review_date',
+            ) as $field_name) {
+              $field = field_view_field('node', $root, $field_name);
+              echo render($field);
+            }
+            echo '</fieldset>';
+          }
 
-      echo render($loop_documents_menu);
-    }
-    ?>
+          echo render($loop_documents_menu);
+        }
+        ?>
 
-    <?php
-    if (isset($loop_documents_roots)) {
-      echo '<h2>' . t('Document collections') . '</h2>';
-      echo render($loop_documents_roots);
-    }
-    ?>
+        <?php
+        if (!empty($loop_documents_roots)) {
+          echo '<h2>' . t('Document collections') . '</h2>';
+          echo render($loop_documents_roots);
+        }
+        ?>
+      </div>
+    <?php endif ?>
   </div>
 
-  <div class="loop-documents--content loop-documents--document-content">
-    <?php include drupal_get_path('theme', 'loop') . '/templates/node/node--page.tpl.php'; ?>
-  </div>
+  <div class="loop-documents--content">
+    <div class="loop-documents--document-content">
+      <?php include drupal_get_path('theme', 'loop') . '/templates/node/node--page.tpl.php'; ?>
+    </div>
 
-	<div class="loop-documents--metadata">
-		<?php
-		$field = field_view_field('node', $node, 'field_loop_documents_author', array('label' => 'hidden'));
-		echo render($field);
-		?>
-	</div>
+    <div class="loop-documents--metadata">
+      <?php
+      $field = field_view_field('node', $node, 'field_loop_documents_author', array('_label' => 'hidden'));
+      echo render($field);
+      ?>
+    </div>
+  </div>
 </div>
