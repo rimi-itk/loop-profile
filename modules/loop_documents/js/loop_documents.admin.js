5ec1e06bd8f71c663e8c2ab19fdbbf9de33d6740
Mikkel Ricky <rimi@aarhus.dk>
2017-01-04 16:58:36 +0100

    LOOP-395: Fixed bug with newly added documents not being shown in menu preview

---
+++ modules/loop_documents/js/loop_documents.admin.js
@@ -0,0 +1,12 @@
+Drupal.ajax.prototype.commands.loop_documents_setDocumentOptions = function(ajax, response, status) {
+    var $ = jQuery;
+    var selector = response.arguments.selector;
+    var options = response.arguments.options;
+
+    var $el = $(selector);
+    $el.empty();
+    $.each(options, function(value, key) {
+        $el.append($("<option></option>")
+                   .attr('value', value).text(key));
+    });
+};
