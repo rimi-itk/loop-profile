1807c3d2b8f2399f16c74c81d92a1dbcf80d90a1
Mikkel Ricky <rimi@aarhus.dk>
2017-01-05 15:08:45 +0100

    LOOP-395: Fixed potential null dereference

--- modules/loop_documents/js/loop_documents.admin.js
+++ modules/loop_documents/js/loop_documents.admin.js
@@ -1,12 +1,14 @@
-Drupal.ajax.prototype.commands.loop_documents_setDocumentOptions = function(ajax, response, status) {
-    var $ = jQuery;
-    var selector = response.arguments.selector;
-    var options = response.arguments.options;
+if (typeof(Drupal.ajax) !== 'undefined') {
+    Drupal.ajax.prototype.commands.loop_documents_setDocumentOptions = function(ajax, response, status) {
+        var $ = jQuery;
+        var selector = response.arguments.selector;
+        var options = response.arguments.options;
 
-    var $el = $(selector);
-    $el.empty();
-    $.each(options, function(value, key) {
-        $el.append($("<option></option>")
-                   .attr('value', value).text(key));
-    });
-};
+        var $el = $(selector);
+        $el.empty();
+        $.each(options, function(value, key) {
+            $el.append($("<option></option>")
+                       .attr('value', value).text(key));
+        });
+    };
+}
