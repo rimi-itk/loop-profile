99459361e93049568c81a9b3441b34539523be17
Mikkel Ricky <rimi@aarhus.dk>
2017-01-23 22:54:28 +0100

    LOOP-404: Improved ui for adding documents to collections

--- modules/loop_documents/js/loop_documents.admin.js
+++ modules/loop_documents/js/loop_documents.admin.js
@@ -1,14 +1,19 @@
 if (typeof(Drupal.ajax) !== 'undefined') {
-    Drupal.ajax.prototype.commands.loop_documents_setDocumentOptions = function(ajax, response, status) {
+    Drupal.ajax.prototype.commands.loop_documents_setValue = function(ajax, response, status) {
         var $ = jQuery;
         var selector = response.arguments.selector;
-        var options = response.arguments.options;
+        var value = response.arguments.value;
 
         var $el = $(selector);
-        $el.empty();
-        $.each(options, function(value, key) {
-            $el.append($("<option></option>")
-                       .attr('value', value).text(key));
-        });
+        $el.val(value);
+    };
+}
+
+// Prevent caching of autocomplete requests (cf. http://drupal.stackexchange.com/a/215761)
+if (typeof(Drupal.ACDB.prototype.search) !== 'undefined') {
+    Drupal.ACDB.prototype.search_original = Drupal.ACDB.prototype.search;
+    Drupal.ACDB.prototype.search = function (searchString) {
+        this.cache = [];
+        this.search_original(searchString);
     };
 }
