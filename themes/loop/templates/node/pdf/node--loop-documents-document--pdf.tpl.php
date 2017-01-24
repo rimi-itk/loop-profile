a88a8d2b5a0d91fffdd172516699ccdfc96e0174
Mikkel Ricky <rimi@aarhus.dk>
2017-01-24 15:44:02 +0100

    LOOP-404: Fixed rendering of children in print view

--- themes/loop/templates/node/pdf/node--loop-documents-document--pdf.tpl.php
+++ themes/loop/templates/node/pdf/node--loop-documents-document--pdf.tpl.php
@@ -8,7 +8,18 @@
 <div class="loop-documents loop-documents--document">
   <div class="loop-documents--content">
     <div class="loop-documents--document-content">
-      <?php include drupal_get_path('theme', 'loop') . '/templates/node/node--page.tpl.php'; ?>
+      <h1 class="page-title loop-documents--document-title">
+        <?php print $title; ?>
+      </h1>
+
+      <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['children']);
+      print render($content);
+      print render($content['children']);
+      ?>
     </div>
   </div>
 </div>
