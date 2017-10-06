0df40845069194e2deef920d5ec64d95ec49a67a
Mikkel Ricky <rimi@aarhus.dk>
2017-10-06 16:26:29 +0200

    SUPPORT-1255: Moved check for multiple submits from client to server

--- themes/loop/scripts/form.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $(function () {
-    // Disable all submit buttons on submit to prevent multiple
-    // submits.
-    $('form.comment-form, form.node-post-form').on('submit', function() {
-      $(this).find(':submit').prop('disabled', true);
-    });
-  });
-})(jQuery);
