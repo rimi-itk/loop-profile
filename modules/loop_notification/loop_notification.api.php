83a6640fc3e6ed912cf08f22cafeb48b109851aa
Mikkel Ricky <rimi@aarhus.dk>
2018-01-22 09:48:21 +0100

    DRIFT-79: Cleaned up code

--- modules/loop_notification/loop_notification.api.php
+++ modules/loop_notification/loop_notification.api.php
@@ -3,24 +3,22 @@
 /**
  * @file
  * Hooks provided by the Notification module.
- * /.
- *
- * /**
+ */
+
+/**
  * @addtogroup hooks
  * @{
  */
 
 /**
- * Implements hook_loop_notification_message_recipient_alter().
- *
  * Modify an array of message recipients before creating a notification.
  *
- * @param $users
+ * @param array $users
  *   An array of user IDs (uid) to receive notification.
- * @param $node
+ * @param object $node
  *   The node that triggered the notification to be send.
  */
-function hook_loop_notification_message_recipient_alter(&$users, $node) {
+function hook_loop_notification_message_recipient_alter(array &$users, $node) {
   $user_ids = array();
   $user_ids[] = '1';
   $users += $user_ids;
